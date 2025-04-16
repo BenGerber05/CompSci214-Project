f9c0b073cd395c933f8ad05c05e86d8495434742
Rourke-N <rourken@icloud.com>
2025-04-16 11:29:35 +0200

    Add files via upload

--- Player/player_2_2.py
+++ Player/player_2_2.py
@@ -1,23 +1,24 @@
-import stddraw, math
+import stddraw
 from picture import Picture
 
 class Player:
+
+    IMAGE_SCALAR = 0.4
+    PINK_SHIP = Picture("Pink_Ship.png") #image
+
     def __init__(self, x, y, fire_rate, health):
         self.x = x
         self.y = y
         self.fire_rate = fire_rate
         self.health = health
-        self.radius = 0.2
+        self.radius = 0.1
         self.x_vel = 0
         self.should_fire = False
-        self.filename = "Pink_Ship.png"
-        self.angle_deg = 0
-        self.original_image = Picture(self.filename)
-        self.rotated_image = self.original_image
 
     def draw(self):
         """Draw the player as a circle."""
-        stddraw.picture(self.rotated_image, self.x, self.y + self.radius, 0.5, 0.6) #changed to show picture
+        
+        stddraw.picture(self.PINK_SHIP, self.x, self.y, self.IMAGE_SCALAR,  self.IMAGE_SCALAR )
 
     def update_position(self):
         """Update player position with velocity, respecting screen bounds."""
@@ -34,7 +35,7 @@ class Player:
         """Set velocity or actions based on key input."""
         key = ""
         if stddraw.hasNextKeyTyped():
-            key = stddraw.nextKeyTyped()
+            key = stddraw.nextKeyTyped().lower()
 
             if key == 'd':
                 self.x_vel = 0.01
@@ -44,12 +45,6 @@ class Player:
                 self.x_vel = 0
             elif key == ' ':
                 self.should_fire = True
-            elif key == "l": #rotate left
-                self.angle_deg += 15       
-            elif key == "k": #rotate right
-                self.angle_deg -= 15
-
-
             elif key == 'q':
                 return False  # signal to quit
 
@@ -61,49 +56,3 @@ class Player:
             self.should_fire = False
             return True
         return False
-
-    def rotate(self):
-        """Rotate ship on l or k keypress. Save rotated ship as picture"""
-
-        #limiting rotation =================== 
-
-        if self.angle_deg >= 75:
-            #undo the change of the angle and pass function
-            self.angle_deg -= 15
-            return
-
-        if self.angle_deg <= -75:
-            #undo the change of the angle and pass function
-            self.angle_deg += 15
-            return
-
-        #rotation =================== 
-        copy_x: int = self.original_image.width() // 2
-        copy_y: int = self.original_image.height() // 2
-
-        angle_rad = math.radians(self.angle_deg) #convert to rad
-        cosMinusTheta: float = math.cos(-angle_rad)
-        sinMinusTheta: float = math.sin(-angle_rad)
-
-        w: int = self.original_image.width() 
-        h: int = self.original_image.height()
-        rotated_image:Picture = Picture(w, h)
-
-        for tx in range(w):
-            for ty in range(h):
-                deltaX: int = tx - copy_x
-                deltaY: int = ty - copy_y
-
-                #formula from tut 3
-                sx: int = int(deltaX*cosMinusTheta - deltaY*sinMinusTheta + copy_x)
-                sy: int = int(deltaX*sinMinusTheta + deltaY*cosMinusTheta + copy_y)
-
-                color: Color = stddraw.BLACK #fill with black if there is no colour
-                if ((sx >= 0) and (sx < w) and (sy >= 0) and (sy < h)): #colour reading is in the image borders
-                    color = self.original_image.get(sx, sy)
-                rotated_image.set(tx, ty, color)
-
-        self.rotated_image = rotated_image
-        #print(f"{self.angle_deg}")
-        
-        return
