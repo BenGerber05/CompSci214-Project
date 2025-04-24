1d2e70cb7ebb5ed7601124596ad7d337cbb9780c
BenGerber05 <bengerber05@gmail.com>
2025-04-24 20:08:43 +0200

    Update defender_4_1.py

--- 4.1/defender_4_1.py
+++ 4.1/defender_4_1.py
@@ -16,6 +16,7 @@ def main():
     pygame.mixer.init()
     shoot_sound = pygame.mixer.Sound("Pew.mp3")
     power_up_sound = pygame.mixer.Sound("PowerUp.mp3")
+    game_over_sound = pygame.mixer.Sound("GameOver.mp3")
     
     enemyMethods = Functions()
         
