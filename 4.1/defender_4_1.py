a86bb1649841809c37720018e0a8d7c7777fd9fe
zakkie123 <zakmuller1@gmail.com>
2025-04-24 18:23:46 +0200

    Update defender_4_1.py

--- 4.1/defender_4_1.py
+++ 4.1/defender_4_1.py
@@ -14,12 +14,12 @@ def main():
     stddraw.setYscale(0, 2)
 
     enemyMethods = Functions()
-    player = Player(x=0, y=0.15, fire_rate=0.45, health=3, angle_deg = 0)
+        player = Player(x=0, y=0.15, fire_rate=0.45, health=3, angle_deg = 0)
     projectiles = []
 
-    player_score = 0
+        player_score = 0
 
-    START_TIME = time.time()
+        START_TIME = time.time()
 
     ENEMY_START_SPAWN_RATE = 0.4
     enemy_spawn_rate = 0
@@ -29,15 +29,15 @@ def main():
 
     enemy_projectiles = []
 
-    lowest_enemy = 0
+        lowest_enemy = 0
     LOWER_BOUND = 0.2
 
     spawn_scale = 0
     speed_scale = 0
 
-    enemy_list = [enemyMethods.spawnRow(ENEMY_START_SPAWN_RATE,enemy_last_fire)]
+        enemy_list = [enemyMethods.spawnRow(ENEMY_START_SPAWN_RATE,enemy_last_fire)]
 
-    lowest_enemy = enemy_list[0][0].y
+    lowest_enemy: float 
 
     shields = []
     
