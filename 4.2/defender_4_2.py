df23671faa26522998f723594939cae81e9201f4
Rourke-N <rourken@icloud.com>
2025-04-24 22:13:20 +0200

    Add files via upload

---
+++ 4.2/defender_4_2.py
@@ -0,0 +1,268 @@
+import stddraw
+from player_4_1 import Player
+from projectile_4_1 import Projectile, Bomb
+from enemy_4_2 import Functions, Bonus, Enemy
+from picture import Picture
+import time
+import math
+import pygame
+
+
+
+def main():
+    
+    stddraw.setXscale(-1, 1)
+    stddraw.setYscale(0, 2)
+
+    pygame.mixer.init()
+    shoot_sound = pygame.mixer.Sound("Pew.mp3")
+    power_up_sound = pygame.mixer.Sound("PowerUp.mp3")
+    bomb_sound = pygame.mixer.Sound("Bomb.mp3")
+    game_over_sound = pygame.mixer.Sound("GameOver.mp3")
+    
+    projectiles = []
+
+
+    highest_score:int = 0
+
+    ENEMY_START_SPAWN_RATE = 0.4
+    enemy_spawn_rate = 0
+    ENEMY_START_SPEED = 0.0025
+    enemy_speed = 0
+    enemy_last_fire = []
+
+    enemy_projectiles = []
+    enemy_list = [[]]
+
+    LOWER_BOUND = 0.2
+
+    spawn_scale = 0
+    speed_scale = 0
+
+
+    lowest_enemy: float
+
+    shields = []
+    
+    print("Game started. Use A/D/S to move, Space to fire, Q to quit.")
+
+    Health_1 = Picture("Lives_1.jpg")
+    Health_2 = Picture("Lives_2.jpg")
+    Health_3 = Picture("Lives_3.jpg")
+
+    GameOver1 = Picture("GAMEOVER_SCREEN1.png")
+    GameOver2 = Picture("GAMEOVER_SCREEN2.png")
+
+    menu1 = True
+    menu2 = False
+    
+     # start page ========================
+    START_PAGE1 = Picture("START_SCREEN1.png")
+    START_PAGE2 = Picture("START_SCREEN2.png")
+    
+    ingame = True
+
+    running = False # dont run main game yet
+
+    while ingame:
+
+        enemyMethods = Functions() 
+        Functions.ref_left = Enemy.HOR_START
+        Functions.ref_right = Enemy.HOR_SPACING*(Enemy.COLUMNS-1) + Enemy.HOR_START    
+        Enemy.ref_direction = 1
+        projectiles.clear()
+        enemy_projectiles.clear()
+        shields.clear()
+        enemy_last_fire.clear()
+
+        lowest_enemy = 0
+        player_score = 0
+        player = Player(x=0, y=0.15, fire_rate=0.45, health=3, angle_deg = 0)
+        enemy_list = [enemyMethods.spawnRow(ENEMY_START_SPAWN_RATE,enemy_last_fire)]
+
+        while menu1:
+            stddraw.clear()
+            stddraw.picture(START_PAGE1, 0, 1, 2.3, 2.3)
+            stddraw.show(150)
+            stddraw.clear()
+            stddraw.picture(START_PAGE2, 0, 1, 2.3, 2.3)
+            stddraw.show(150)
+
+            if stddraw.hasNextKeyTyped():
+                key = stddraw.nextKeyTyped().lower() # account for caps
+                if key == 'e':
+                    running = True # only start main game loop now
+                    start_time = time.time() 
+                    menu1 = False
+                    ingame = False
+                if key == 'q':
+                    menu1 = False
+                    ingame = False
+
+        # start page ========================
+
+
+        while running:
+     
+            current_time = time.time() - start_time
+            
+            spawn_scale = math.log(current_time/400 + 1) #scales such that spawn rate increases logarithmically and reaches 1 after around 5 minutes
+            
+            enemy_spawn_rate = ENEMY_START_SPAWN_RATE + spawn_scale 
+     
+            if enemy_speed <= 0.006:
+                speed_scale = 0.005*math.pow(math.e,current_time/700) - 0.005 #scales exponentially such that the speed reaches its maximum of 0.006 after around 5 minutes
+                enemy_speed = ENEMY_START_SPEED + speed_scale
+
+            stddraw.clear(stddraw.BLACK)
+            player.update_position()
+            running = player.handle_key()
+            player.draw()
+
+            #health hud ===============
+            if player.health == 3:
+                stddraw.picture(Health_3, 0.7,1.9,0.5,0.15)
+            elif player.health == 2:
+                stddraw.picture(Health_2, 0.7,1.9,0.5,0.15)
+            elif player.health == 1:                
+                stddraw.picture(Health_1, 0.7,1.9,0.5,0.15)
+            # =========================
+            
+            # Fire projectile Test: 20:52
+            if player.wants_to_fire() :
+                shoot_sound.play()
+                if Bonus.powerUp == "BOOM":
+                    proj = Bomb(player.x, player.y + player.radius, player.angle_deg)
+                else:
+                    proj = Projectile(player.x, player.y + player.radius, player.angle_deg)
+                projectiles.append(proj)
+            
+            # Show score
+            stddraw.setFontSize(s=22)
+            stddraw.setFontFamily(f="Arial")
+            stddraw.setPenColor(stddraw.WHITE)
+            stddraw.text(-0.8, 1.93, "Score: " + str(player_score))
+
+            #Start of change
+            #Put shield hit checking in its own loop
+            for shield in shields:
+                 shield.draw()
+                 for proj in projectiles[:]:
+                     if shield.is_hit(proj):
+                         projectiles.remove(proj)
+                         shield.health-=1
+                         if shield.health == 0:
+                             shields.remove(shield)
+                 for proj in enemy_projectiles[:]:
+                     if shield.is_hit(proj):
+                         enemy_projectiles.remove(proj)
+                         shield.health-=1
+                         if shield.health == 0:
+                             shields.remove(shield)
+            #End of change
+            
+            # Update and draw enemy projectiles and check player for hits
+            for proj in enemy_projectiles[:]:
+                proj.update()
+                proj.draw()
+
+                if player.is_hit(proj):
+                    enemy_projectiles.remove(proj)
+                    player.health -= 1
+                    player.fire_rate = 0.45
+                    if player_score>0:
+                        player_score -= 100
+                    if player.health <= 0: # player dies
+                        game_over_sound.play() 
+                        running = False
+                        menu2 = True
+                        ingame = True
+                        if player_score > highest_score:
+                            highest_score = player_score
+
+                        for row in enemy_list:
+                            for enemy in row:
+                                row.remove(enemy)
+                                if len(row) == 0:
+                                    enemy_list.remove(row)
+
+                        break
+                        
+
+                if proj.is_off_screen() and proj in enemy_projectiles:
+                    enemy_projectiles.remove(proj)
+            
+            # Update and draw projectiles
+            for proj in projectiles[:]:
+                proj.update()
+                proj.draw()
+
+                # Check collision with enemies
+                count = 0
+                for row in enemy_list:
+                    for enemy in row:
+                        if enemy.is_hit(proj):
+                            enemy.health-=1
+                            projectiles.remove(proj)
+                            if enemy.health <= 0:
+                                bomb_sound.play()
+                                player_score += enemy.score()
+                                if isinstance(enemy, Bonus): #drop power up if a bonus enemy is killed
+                                    power_up_sound.play()
+                                    enemy.powerUp(player,shields)
+                                row.remove(enemy)
+                                if len(row) == 0:
+                                    enemy_list.remove(row)
+                                    enemy_last_fire.pop(count) 
+                    count +=1                     
+                   
+                if proj.is_off_screen() and proj in projectiles:
+                    projectiles.remove(proj)
+
+            enemyMethods.move(enemy_list, enemy_spawn_rate, enemy_speed,enemy_last_fire, enemy_projectiles)
+
+            if len(enemy_list)>0 and len(enemy_list[0])>0:
+                lowest_enemy = enemy_list[0][0].y
+                if lowest_enemy <= LOWER_BOUND:
+                    running = False
+                    break
+            
+                     
+            stddraw.show(20)
+        stddraw.setPenColor(stddraw.WHITE)
+        while menu2:
+            #display endmenu
+            pygame.mixer.music.load("GameOver.mp3")
+            pygame.mixer.music.set_volume(0.5)
+            stddraw.clear()
+            stddraw.picture(GameOver1, 0, 1, 2.3, 2.3)
+            stddraw.text(0, 0.75, str(player_score))
+            stddraw.text(0, 1.1, str(highest_score))
+            stddraw.show(150)
+
+            stddraw.clear()
+            stddraw.picture(GameOver2, 0, 1, 2.3, 2.3)
+            stddraw.text(0, 0.75, str(player_score))
+            stddraw.text(0, 1.1, str(highest_score))
+            stddraw.show(150)
+
+            if stddraw.hasNextKeyTyped():
+                key = stddraw.nextKeyTyped().lower() # account for caps
+                if key == 'e':
+                    running = True # only start main game loop now
+                    menu2 = False
+                    ingame = True
+                if key == 'q':
+                    menu2 = False
+                    ingame = False
+
+
+        
+        
+
+
+        
+        
+
+if __name__ == '__main__':
+    main()
---
+++ 4.2/shield_4_1.py
@@ -0,0 +1,36 @@
+import stddraw
+from projectile_4_1 import Projectile
+
+
+class Shield:
+    
+    START_HEALTH = 4
+
+    def __init__(self, position): 
+
+        self.width = 0.2
+        self.height = 0.1
+
+        self.x = position*0.5 - self.width/2
+        self.y = 0.5
+
+        self.position = position
+
+        self.health = 4
+
+    def draw(self):
+        stddraw.setPenColor(stddraw.BLUE)
+        stddraw.filledRectangle(self.x, self.y, self.width, self.height) 
+           
+        if self.health < Shield.START_HEALTH:
+
+            width = self.width*(Shield.START_HEALTH-self.health)/Shield.START_HEALTH
+            height = self.height*(Shield.START_HEALTH-self.health)/Shield.START_HEALTH
+            x = self.x + (self.width - width)/2
+            y = self.y + (self.height - height)/2
+
+            stddraw.setPenColor(stddraw.BLACK)
+            stddraw.filledRectangle(x, y,width , height)
+
+    def is_hit(self, projectile: Projectile):
+        return projectile.x>=self.x and projectile.x<=self.x + self.width and projectile.y >=self.y and projectile.y<=self.y + self.height
